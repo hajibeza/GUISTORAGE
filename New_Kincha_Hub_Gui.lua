6aaa9a03ab766eb03be2e594fbc158cb32286efa
hajibeza <74174244+hajibeza@users.noreply.github.com>
2023-01-28 07:57:47 +0700

    Update New_Kincha_Hub_Gui.lua

--- New_Kincha_Hub_Gui.lua
+++ New_Kincha_Hub_Gui.lua
@@ -15,7 +15,7 @@ local library = {
 		easingDirection = Enum.EasingDirection.Out
 	},
 	colors = {
-		main = Color3.fromRGB(255, 39, 39),
+		main = Color3.fromRGB(15, 132, 27),
 		background = Color3.fromRGB(40, 40, 40),
 		outerBorder = Color3.fromRGB(15, 15, 15),
 		innerBorder = Color3.fromRGB(73, 63, 73),

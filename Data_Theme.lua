f384b65cbc1c7c09eaa675d8b804a25e99d75415
hajibeza <74174244+hajibeza@users.noreply.github.com>
2024-11-11 21:39:27 +0700

    Update Data_Theme.lua

--- Data_Theme.lua
+++ Data_Theme.lua
@@ -8,7 +8,7 @@ local ThemeManager = {} do
 
 	ThemeManager.Library = nil
 	ThemeManager.BuiltInThemes = {
-		['Default'] 		= { 1, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"2589f5","AccentColor":"00ff6d","BackgroundColor":"000000","OutlineColor":"395843"}') },
+		['Default'] 		= { 1, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"0e0d0d","AccentColor":"2589f5","BackgroundColor":"000000","OutlineColor":"395843"}') },
 		['Green'] 			= { 2, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"141414","AccentColor":"00ff8b","BackgroundColor":"1c1c1c","OutlineColor":"3c3c3c"}') },
 		['Jester'] 			= { 3, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"242424","AccentColor":"db4467","BackgroundColor":"1c1c1c","OutlineColor":"373737"}') },
 		['Mint'] 			= { 4, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"242424","AccentColor":"3db488","BackgroundColor":"1c1c1c","OutlineColor":"373737"}') },
